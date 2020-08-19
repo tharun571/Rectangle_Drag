12c884af53329888c6fa38259faf20e9d9372d27
tharun571 <63015429+tharun571@users.noreply.github.com>
2020-08-19 13:45:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rectangle_drag/Canvas.java
@@ -0,0 +1,128 @@
+package com.example.rectangle_drag;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class Canvas extends View {
+
+    private static final String TAG = "QWERTYUI";
+    float left=100, right=600, top=100, bottom=500;
+    float b,h;
+    public Canvas(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public Canvas(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+
+    }
+
+    public Canvas(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+
+    }
+
+    public Canvas(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+
+    }
+
+    private void init(@Nullable AttributeSet set){
+
+        postInvalidate();
+    }
+
+    @Override
+    protected void onDraw(android.graphics.Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.parseColor("#000000"));
+
+        canvas.drawRect(left,top,right,bottom,paint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean value = super.onTouchEvent(event);
+
+        Log.w(TAG,"QWE");
+
+        switch (event.getAction()){
+
+            case MotionEvent.ACTION_DOWN:{
+                return true;
+            }
+
+            case MotionEvent.ACTION_MOVE:{
+                float x=event.getX();
+                float y=event.getY();
+                Log.w(TAG,"QWE 1 "+x+" "+y);
+
+                if(x>left+30&&x<right-30&&y>top+30&&y<bottom-30){
+                    b=(right-left)/2;
+                    h=(bottom-top)/2;
+
+
+                    left=x-b;
+                    right=x+b;
+                    top=y-h;
+                    bottom=y+h;
+
+                    postInvalidate();
+
+
+                    return true;
+                }
+                else if(x>left-30&&x<left+30&&y<bottom&&y>top){
+                    left=x;
+                    postInvalidate();
+                    return true;
+
+
+                }
+                else if(x>right-30&&x<right+30&&y<bottom&&y>top){
+                    right=x;
+                    postInvalidate();
+                    return true;
+
+
+                }
+                else if(x>left&&x<right&&y<bottom+30&&y>bottom-30){
+                    bottom=y;
+                    postInvalidate();
+                    return true;
+
+
+                }
+                else if(x>left&&x<right&&y<top+30&&y>top-30){
+                    bottom=y;
+                    postInvalidate();
+                    return true;
+
+
+                }
+
+
+                return value;
+        }
+        }
+
+
+        return value;
+
+
+    }
+}
